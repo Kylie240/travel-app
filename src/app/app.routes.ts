a3aa726f4a24eca335ccd73886c7a4dba6dacda5
Kylie <113202977+Kylie240@users.noreply.github.com>
2025-05-01 20:22:19 -0400

    navbar user menu, create listing and account page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ExplorePageComponent } from './pages/explore-page/explore-page.componen
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ListingComponent } from './pages/listing/listing.component';
+import { CreateListingComponent } from './pages/create-listing/create-listing.component';
+import { AccountComponent } from './pages/account/account.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
@@ -11,4 +13,6 @@ export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'listing', component: ListingComponent },
+    { path: 'account', component: AccountComponent },
+    { path: 'create-listing', component: CreateListingComponent },
 ];
--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -8,6 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputOtpModule } from 'primeng/inputotp';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-navbar',
@@ -22,13 +24,17 @@ import { InputOtpModule } from 'primeng/inputotp';
     AutoCompleteModule,
     InputNumberModule,
     InputOtpModule,
+    Menu,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
   isHomepage = false;
+  items: any[] = [];
+  menuItems: MenuItem[] = [];
   visible = false;
+  value: any;
   showSignUp = false;
   showLogIn = false;
   showOTC = false;
@@ -41,13 +47,42 @@ export class NavbarComponent implements OnInit {
   }
 
 ngOnInit(): void {
+  this.menuItems = [
+    {
+        items: [
+            { label: 'Messages'},
+            { label: 'Trips'},
+            { label: 'Wishlist'},
+        ]
+    },
+    {
+        items: [
+            { label: 'Create Itenerary', routerLink: '/create-listing'},
+            { label: 'Manage Listings'},
+            { label: 'Account', routerLink: '/account'},
+        ]
+    },
+    {
+        items: [
+            { label: 'Gift Cards'},
+            { label: 'Help Center'},
+            { label: 'Log Out'},
+        ]
+    }
+];
   // let test = document.getElementById
-  this.getUser;
+  this.user ={
+    name: 'Kylie',
+    email: 'olivkylie@gmail.com',
+    phone: '9545945535',
+    image: 'https://media.licdn.com/dms/image/v2/D4E03AQH1o4Avl01RCA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1698873322772?e=2147483647&v=beta&t=4lwjMLSYjyH9c528Y9FQZXn_tqii1bFYpjSJ2v3VOX4',
+    reviews: 3289,
+    rating: 4.59,
+    joined: new Date(),
+    title: 'Solo Travel Enthusiast',
+    about: 'My listings are mainly full of solor travel plans that allow other women of color to travel comfortably and affordbly to places outside of the U.S.',
+  };
 }
-
-items: any[] = [];
-
-  value: any;
 
   search(event: AutoCompleteCompleteEvent) {
   let _items = [...Array(10).keys()];
